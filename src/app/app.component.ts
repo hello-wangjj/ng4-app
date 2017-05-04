c60f007febefd68e9c33b529951f5e5b285ff91e
wangjj <wangjj886688@163.com>
2017-05-04 22:20:48 +0800

    完善

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Hero} from './hero';
+import {HeroService} from "./shared/hero.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) {
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
+  onSelected(hero): void {
+    this.selectedHero = hero;
+  };
+  getHeroes(): void {
+    this.heroService.getHeroes().then(
+      heroes => this.heroes = heroes
+    );
+  }
 }
